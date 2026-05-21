f81c9a3053c5a922036f4bceac740e354e3797df
Evan Conway <evanconwaymusic@gmail.com>
2026-05-20 22:32:50 -0400

    remove messages idea

--- src/gameplay/WithEnemies.ts
+++ src/gameplay/WithEnemies.ts
@@ -1,14 +1,9 @@
 import { GameplayController } from "src/controllerManager";
 import { Gameplay } from "./Gameplay";
 
-const frameTimeUnit = (1 / 60) * 1000;
-
-export class UsingMessage extends Gameplay {
-  private time: number;
-
+export class WithEnemies extends Gameplay {
   constructor() {
     super();
-    this.time = 0;
   }
 
   update(input: GameplayController, timePassed: number): void {}
